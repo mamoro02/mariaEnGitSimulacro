6ef80e57200242092261a6833843632f41fc4904
mamoro02 <mamoro02@floridauniversitaria.com>
2023-02-09 18:16:22 +0100

    ej c

--- c.sh
+++ c.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 matriculas=`cat ./ejercicioEX/notas.txt | wc -l`
-
+aprobado=0
+suspendido=0
 for i in `seq 2 $matricula`
 do
 linea=`cat ./ejercicioEX/notas.txt | head -n$i | tail -n1 | awk '{print $3}'`
+if [ $linea -ge 5 ]; then
+aprobado=$((aprobado+1))
+else
+suspendido=$((suspendido+1))
+fi
+done
+
+echo "El numero de suspendidos es de $suspendo personas."
+echo "El numero de suspendidos es de $aprobado personas."
+
+sleep 5
