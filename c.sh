b164f376ffa5e601ea8429263b48c3f58a090ecf
mamoro02 <mamoro02@floridauniversitaria.com>
2023-02-09 18:43:50 +0100

    act c

--- c.sh
+++ c.sh
@@ -3,7 +3,7 @@
 matriculas=`cat ./ejercicioEX/notas.txt | wc -l`
 aprobado=0
 suspendido=0
-for i in `seq 2 $matricula`
+for i in `seq 2 $matriculas`
 do
 linea=`cat ./ejercicioEX/notas.txt | head -n$i | tail -n1 | awk '{print $3}'`
 if [ $linea -ge 5 ]; then
@@ -13,7 +13,7 @@ suspendido=$((suspendido+1))
 fi
 done
 
-echo "El numero de suspendidos es de $suspendo personas."
-echo "El numero de suspendidos es de $aprobado personas."
+echo "El numero de suspendidos es de $suspendido personas."
+echo "El numero de aprobado es de $aprobado personas."
 
 sleep 5
